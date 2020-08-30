7a36468e46d0a462d63de9a9560c7c205854a093
Jose <pastorpedraza3@gmail.com>
2020-08-30 17:49:21 -0500

    change bot link

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,7 @@ require_relative '..\lib\bot.rb'
 
 puts '----------Welcome to Telegram bot map-bot -----------'
 puts ' '
-puts "go to https://web.telegram.org/#/im?p=@josepedeaza_bot to open telegram web app"
+puts "go to https://web.telegram.org/#/im?p=@josepedraza_bot to open telegram web app"
 puts 'but running.....'
 puts 'Type Ctrl+C to stop the bot'
 
