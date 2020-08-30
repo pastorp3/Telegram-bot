80d0159d43bb98e085911ae5681b3071ac6085a5
Jose <pastorpedraza3@gmail.com>
2020-08-30 14:51:04 -0500

    add bin and lib folder

---
+++ bin/main.rb
@@ -0,0 +1,8 @@
+require_relative 'bot.rb'
+
+puts '----------Welcome to Telegram bot map-bot -----------'
+puts ' '
+puts "go to https://web.telegram.org/#/im?p=@josepedeaza_bot to open telegram web app"
+puts 'but running.....'
+
+Bot.new
